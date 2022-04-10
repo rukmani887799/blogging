b13588c5d8ab7ce0dcf871d0d796f7c73da4131c
rukmani887799 <101476931+rukmani887799@users.noreply.github.com>
2022-04-11 01:44:51 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { resolve } from 'path';
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-cloudflare';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +11,7 @@ const config = {
 	}),
 
 	kit: {
-		adapter: adapter({
+		adapter: vercel({
 			pages: 'build/frontend',
 			assets: 'build/frontend',
 			fallback: null
